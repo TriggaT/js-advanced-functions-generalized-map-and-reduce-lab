33ff8397d960f884e9a8f3d8597d651779702ba5
Trayshawn <traytwebb14@yahoo.com>
2021-02-27 20:06:16 -0600

    commit tree ef1b1dfdc93c128fa43a925ddc3c64847044e57b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+
+function map(array, work){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(work(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, work, startingPoint = 0){
+    let a = (!!startingPoint) ? startingPoint : array[0] 
+    let i = (!!startingPoint) ? 0 : 1
+    for (i; i < array.length; i++){
+        a = work(array[i], a)
+    }
+    return a
+
+}
